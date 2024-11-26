ae3bdcc72399d38bd9585d15912103a3a3f6de3f
anniew03 <138319806+anniew03@users.noreply.github.com>
2024-11-26 14:46:27 -0500

    Update user_actions.py

--- app/models/user_actions.py
+++ app/models/user_actions.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
-
 from typing import Optional
-
 from pydantic import BaseModel
+from datetime import datetime
 
 class Like(BaseModel):
     user_id: int
@@ -13,24 +12,9 @@ class Comment(BaseModel):
     user_id: int
     recipe_id: int
     content: str
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
 
 class Follow(BaseModel):
     follower_id: int
     following_id: int
-
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "course_id": 204283,
-                "course_name": "COMSW4153_001_2024_3 - Cloud Computing",
-                "uuid": "3jHCxUV0ck9Z8TF1sZeI8WTx47olDGkX1YPL3USM",
-                "created_at": "2024-04-05T00:58:50Z",
-                "course_code": "COMSW4153_001_2024_3 - Cloud Computing",
-                "sis_course_id": "COMSW4153_001_2024_3",
-                "course_no": "COMSW4153",
-                "section": "001",
-                "course_year": "2024",
-                "semester": "3"
-            }
-        }
